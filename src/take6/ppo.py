8d3e388328e043894179ed6501fbe73cdb7afd1b
Giovanni GATTI PINHEIRO <giovanni.gattipinheiro@amadeus.com>
2022-10-10 17:08:39 +0200

    Saving ratings when checking point

--- src/take6/ppo.py
+++ src/take6/ppo.py
@@ -1,5 +1,10 @@
+import os
+import pathlib
+import pickle
+from collections import defaultdict
 from typing import Optional, Union, Callable
 
+import trueskill
 from ray.rllib.algorithms import ppo, AlgorithmConfig
 from ray.rllib.utils.metrics import NUM_AGENT_STEPS_SAMPLED
 from ray.rllib.utils.typing import ResultDict, PartialAlgorithmConfigDict, EnvType
@@ -34,3 +39,18 @@ class TimedPPO(ppo.PPO):
         _result[result.TIMESTEPS_THIS_ITER] = num_agent_steps_sampled - self._num_agent_steps_sampled
         self._num_agent_steps_sampled = num_agent_steps_sampled
         return _result
+
+    def save_checkpoint(self, checkpoint_dir: str) -> str:
+        saved = super().save_checkpoint(checkpoint_dir)
+        if hasattr(self, 'ratings'):
+            ratings_file = os.path.join(checkpoint_dir, 'ratings')
+            pickle.dump(dict(self.ratings), open(ratings_file, 'wb'))
+        return saved
+
+    def load_checkpoint(self, checkpoint_path: str) -> None:
+        super().load_checkpoint(checkpoint_path)
+        parent = pathlib.Path(checkpoint_path).parent
+        ratings_file = pathlib.Path(parent, 'ratings')
+        original = pickle.load(open(ratings_file, 'rb'))
+        trueskill.setup(mu=25., sigma=25. / 3, beta=20.8, tau=25. / 100, draw_probability=0.18)
+        self.ratings = defaultdict(lambda: trueskill.Rating(), original)
