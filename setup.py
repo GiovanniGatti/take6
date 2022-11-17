d498ab89fb74f9175e5ce1b0897eba1d5b3ffb7c
Giovanni GATTI PINHEIRO <giovanni.gattipinheiro@amadeus.com>
2022-11-17 11:32:04 +0100

    Fixing missing dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-try:
-    import tensorflow
-except ImportError as e:
-    raise ModuleNotFoundError('Expected tensorflow>=2.2.0 to be provided')
-
 setup(name='take6',
       version='0.1.0-SNAPSHOT',
       author='Giovanni Gatti Pinheiro',
@@ -15,5 +10,5 @@ setup(name='take6',
       packages=['take6'],
       package_data={'take6': ['py.typed']},
       install_requires=['azureml-core==1.35.0', 'azureml-train-core==1.35.0', 'ray[rllib]==2.0.0', 'gym==0.21.0',
-                        'trueskill==0.4.5'],
+          'trueskill==0.4.5', 'inquirer==2.10.0', 'tensorflow==2.4.3'],
       python_requires='>=3.7')
